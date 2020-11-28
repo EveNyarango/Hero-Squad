2bb115d4c67660566b231b04fb537f053047b2a9
EveNyarango <evenyarango@gmail.com>
2020-11-28 19:00:06 +0300

    add and performed functionality and hero in the squad test

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -18,5 +18,15 @@ public class HeroTest {
     squad.add(hero);
     assertEquals(2, Squad.getMembers().size());
 }
-
+@Test
+    public void functionalityTest(){
+    Hero king = new Hero("Nyarango", 60, "Superman", "Stubborn");
+    assertTrue(Hero.getAll().contains(king));
+}
+@Test
+    public void heroInTheSquad(){
+    Hero squadHero = new Hero("Wakanda",36,"Vibranium", "Nerdy");
+    Squad.add(squadHero);
+    assertTrue(Squad.whoIsTheHeroInSquad(squadHero));
+}
 }
