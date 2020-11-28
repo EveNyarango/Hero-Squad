60d56770e72a1e2a0ffafbcf11c9536836782243
EveNyarango <evenyarango@gmail.com>
2020-11-28 23:08:24 +0300

    add routing path for squadform

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,32 @@
+import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 public class App {
+public static void main (String[] args){
+    staticFileLocation("/public");
+
+
+//    route for creating squad
+    post("/success", (request, response) ->{
+        String name = request.queryParams("name");
+        int size = Integer.parseInt(request.queryParams("size"));
+        String cause = request.queryParams("cause");
+
+        Squad squad = new Squad(size, name, cause);
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("squad", squad);
+        return new ModelAndView(model, "success.hbs");
+    }, new HandlebarsTemplateEngine());
+
+
+
+
+}
 
 }
