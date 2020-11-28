11897890998c10e951056133c3934d40aa17a576
EveNyarango <evenyarango@gmail.com>
2020-11-28 16:50:56 +0300

    add constructors,getter and hero list

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,5 +1,52 @@
 package models;
 
-public class Hero {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Hero {
+private String name;
+private int age;
+private String power;
+private String weaknesses;
+    private static List<Hero> hInstances = new ArrayList<>();
+private int size;
+
+    public Hero(String name, int age, String power, String weaknesses, int size) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weaknesses = weaknesses;
+        hInstances.add(this);
+        size = hInstances.size();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public String getWeaknesses() {
+        return weaknesses;
+    }
+
+    public static List<Hero> getAll() {
+        return hInstances;
+    }
+
+    public static List<Hero> addHero() {
+        Hero hero = new Hero("Aquaman", 40, "Ability to communicate with creatures", "kryptonite");
+        Hero iron = new Hero("Ironman", 44, "Supersonic flight Energy", "Ego-Centric");
+        Hero king = new Hero("Wakanda",36,"Vibranium", "Nerdy");
+        hInstances.add(hero);
+        hInstances.add(iron);
+        hInstances.add(king);
+        return hInstances;
+    }
 }
